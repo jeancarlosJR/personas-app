d812d72efaa9ba9f422f85aa99800dea15cb0fd4
JeancarlosJR <jcqm757@gmail.com>
2024-04-03 20:36:20 -0500

    Municipio Edit

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -36,4 +36,53 @@ class MunicipioController extends Controller
         return view('municipio.new', ['departamentos' => $departamentos]);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $municipio = new Municipio();
+
+        $municipio->muni_nomb = $request->name;
+        $municipio->depa_codi = $request->code;
+        $municipio->save();
+
+        $municipios = DB::table('tb_municipio')
+        ->join('tb_departamento', 'tb_municipio.depa_codi', '=' , 'tb_departamento.depa_codi')
+        ->select('tb_municipio.*' , "tb_departamento.depa_nomb")
+        ->get();
+
+        return view('municipio.index', ['municipios' => $municipios]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $municipio = Municipio::find($id);
+        $departamentos = DB::table('tb_departamento')
+        ->orderBy('depa_nomb')
+        ->get();
+
+        return view ('municipio.edit' , ['municipio' => $municipio, 'departamentos' => $departamentos]);
+    }
 }
