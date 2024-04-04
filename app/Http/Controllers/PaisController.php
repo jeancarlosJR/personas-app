00ca6245fed9359ca0aeb4dde5f5a5ac6ad675af
JeancarlosJR <jcqm757@gmail.com>
2024-04-03 20:46:05 -0500

    Pais Edit

--- app/Http/Controllers/PaisController.php
+++ app/Http/Controllers/PaisController.php
@@ -37,4 +37,54 @@ class PaisController extends Controller
         return view ('pais.new' , ['municipios' => $municipios]);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $pais = new Pais();
+
+
+        $pais->pais_nomb= $request->name;
+        $pais->muni_codi= $request->code;
+        $pais->save();
+
+        $paises = DB::table('tb_pais')
+        ->join('tb_municipio', 'tb_pais.pais_capi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_pais.*', 'tb_municipio.muni_nomb')
+        ->get();
+
+        return view('pais.index', ['paises' =>$paises]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $pais = Pais::find($id);
+        $municipios = DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+
+        return view ('pais.edit' , ['pais' => $pais, 'municipios' => $municipios]);
+    }
+
 }
