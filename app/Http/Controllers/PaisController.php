17b94f7b02e581c06ea5111da57ae94bb405570f
JeancarlosJR <jcqm757@gmail.com>
2024-04-03 20:45:21 -0500

    Pais Add

--- app/Http/Controllers/PaisController.php
+++ app/Http/Controllers/PaisController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pais;
+use Illuminate\Support\Facades\DB;
 
 class PaisController extends Controller
 {
@@ -13,7 +15,12 @@ class PaisController extends Controller
      */
     public function index()
     {
-        //
+        $paises = DB::table('tb_pais')
+          ->join('tb_municipio', 'tb_pais.pais_capi', '=', 'tb_municipio.muni_codi')
+          ->select('tb_pais.*', 'tb_municipio.muni_nomb')
+          ->get();
+        return view('pais.index' , ['paises' =>$paises]);
+
     }
 
     /**
@@ -23,62 +30,11 @@ class PaisController extends Controller
      */
     public function create()
     {
-        //
+        $municipios = DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+
+        return view ('pais.new' , ['municipios' => $municipios]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
