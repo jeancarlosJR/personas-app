8c7a2493ee3a05d184964a9eacff7d8a7161a93f
JeancarlosJR <jcqm757@gmail.com>
2024-04-03 20:24:56 -0500

    Comuna Delete

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -111,4 +111,23 @@ class ComunaController extends Controller
         return view ('comuna.index' , ['comunas' => $comunas]);
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $comuna = Comuna::find($id);
+        $comuna->delete();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=' , 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+        ->get();
+
+        return view ('comuna.index' , ['comunas' => $comunas]);
+    }
 }
+
